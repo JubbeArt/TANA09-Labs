9d25d97e872bf6afc70454f678dd0c20ef3a7497
Jesper Wrang <jeswr740@student.liu.se>
2018-12-14 11:38:02 +0100

    Added some answers to lab 3

--- lab3/2/u2.m
+++ lab3/2/u2.m
@@ -9,12 +9,6 @@ p13 = polyval(p, 1.3);
 
 %f = @(x) exp(-x/5)+sin(x);
 
-% 2.1 - You need 2 points. Use the first two since 1.3 is between them.
-% P1(1.3) = 1.7071
-% 2.2
-% 2.3 - Worse approximations, larger errors
-% 2.4 - Don't use too high polynomials for interpolation
-
 % polyval(p, 1.3) - polyval(p2, 1.3)
 
 f13 = exp(-1.3/5) + sin(1.3);
---
+++ lab3/README.md
@@ -0,0 +1,14 @@
+# 2 Polynomial interpolation
+
+## 2.1 
+We need two points, use the first two since 1.3 is in that interval.
+
+## 2.2
+
+P<sub>1</sub>(1.3) = 1.7071
+
+## 2.3
+
+| R<sub>T</sub> | ≤ 0.284 * 10<sup>-1<sup>
+
+| R<sub>XF</sub> | ≤ 0.5 * 10<sup>-4<sup>
