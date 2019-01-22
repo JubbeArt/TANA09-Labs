7833815c12bd0d542015064ae6f78845e24c5ed8
Jesper Wrang <jeswr740@student.liu.se>
2019-01-22 16:35:08 +0100

    Removed old matlab files for lab 1

--- lab1/CalcDiv.m
+++
@@ -1,7 +0,0 @@
-err = [];
-i = 1;
-
-for x = 1:0.01:2
-   err(i) = abs(Division(x) - 1.32/x);
-   i = i + 1;
-end
--- lab1/hmmm.m
+++
@@ -1,5 +0,0 @@
-fplot(@(x) Division(x), [1 2], '--or')
-hold on
-fplot(@(x) 1.32/x, [1 2], '-.*c')
-hold off
-grid on
